4d50577dae9bcb15d1d81d5b622195cd604cdd7e
Federico-I <threxos.48@gmail.com>
2024-01-29 23:19:09 +0000

    customizing-comp-Props

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    {/*<App />*/}
-    <StartRating />
+    <StartRating maxRating={5} messagess={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
+    <StartRating size={24} color="red" className="test"/>
   </React.StrictMode>
 );
 
