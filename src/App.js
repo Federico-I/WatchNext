77c08ce3bf2027cea4d24ebd9bb9e4e61f39af85
Federico-I <threxos.48@gmail.com>
2024-05-14 22:28:19 +0100

    spliting-components

--- src/App.js
+++ src/App.js
@@ -47,27 +47,20 @@ const tempWatchedData = [
   },
 ];
 
-//////////////////////////////////////////////////////////
-//                       APP
-//////////////////////////////////////////////////////////
+
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-export default function App() {
+//////////////////////////////////////////////////////////
+//                       NavBar
+//////////////////////////////////////////////////////////
+
+function NavBar() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
-
-  return (
-    <>
-      <nav className="nav-bar">
+  return(
+    <nav className="nav-bar">
         <div className="logo">
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
@@ -80,9 +73,30 @@ export default function App() {
           onChange={(e) => setQuery(e.target.value)}
         />
         <p className="num-results">
-          Found <strong>{movies.length}</strong> results
+          Found <strong>X</strong> results
         </p>
       </nav>
+  )
+};
+
+
+//////////////////////////////////////////////////////////
+//                       Main
+//////////////////////////////////////////////////////////
+function Main() {
+  
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <>
+      <NavBar />
 
       <main className="main">
         <div className="box">
@@ -169,7 +183,22 @@ export default function App() {
       </main>
     </>
   );
-}
+};
+
+//////////////////////////////////////////////////////////
+//                       APP
+//////////////////////////////////////////////////////////
+
+export default function App() {
+
+  return(
+    <>
+      <NavBar />
+      <Main />
+    </>
+  )
+
+};
 
 
 
